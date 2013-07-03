2a353f0401cd83b62099f277b271414b0cb91806
Matthew Bunday <zenceph@gmail.com>
2013-07-03 16:48:24 -0400

    Fixing the dumb way cinch does pattern matching.

--- irc_bot.rb
+++ irc_bot.rb
@@ -13,15 +13,15 @@ bot = Cinch::Bot.new do
       QUOTES = File.open('quotes.txt').readlines
   end
 
-  on :message, /encourage/ do |m|
-      m.reply "#{m.user.nick}: " + QUOTES.sample(1).first
-  end
-
   on :message, /.*/ do |m|
       if m.message.match(/^#{m.bot.nick}:/)
-          msg = m.message.gsub(/^#{m.bot.nick}:/, '')
-          response = ALIZA.think(msg)
-          m.reply "#{m.user.nick}: " + response.gsub(/cleverbot/i, m.bot.nick) unless response.nil?
+          if m.message.match(/encourage/)
+              m.reply "#{m.user.nick}: " + QUOTES.sample(1).first
+          else
+              msg = m.message.gsub(/^#{m.bot.nick}:/, '')
+              response = ALIZA.think(msg)
+              m.reply "#{m.user.nick}: " + response.gsub(/cleverbot/i, m.bot.nick) unless response.nil?
+          end
       end
   end
 end
